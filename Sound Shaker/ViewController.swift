9af68247d517c21155cd3598a2a75f9840955bee
Yohannes Wijaya <yohannes.wijaya@gmail.com>
2015-07-30 15:22:03 +0700

    Added: gesture for play, play random, and pause. Also, initiated audio player, logic for play & pause functions. Todo: logic for previous, next, and random song playback.

--- Sound Shaker/ViewController.swift
+++ Sound Shaker/ViewController.swift
@@ -7,71 +7,130 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        self.albumArtImageView.userInteractionEnabled = true
-        let swipeDirections: [UISwipeGestureRecognizerDirection] = [.Down, .Left, .Right]
-        for swipeDirection in swipeDirections {
-            let gesture = UISwipeGestureRecognizer(target: self, action: "validateSwipeGesture:")
-            gesture.direction = swipeDirection
-            self.view.subviews[0].addGestureRecognizer(gesture)
-        }
+        self.randomSongIndex = Int(arc4random_uniform(UInt32(self.songFilesPath.count)))
+        self.initiateSoundPlayer()
+        self.initiateTapGestureToPlayOrPauseSong()
+        self.initiateSwipeRelatedGesturesToControlSongPlayback()
+        self.initiateTapAndHoldGestureToStopSong()
+        
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
+    
+    // MARK: - Stored properties
+    
+    var songPlayer: AVAudioPlayer!
+    let songFilesPath: [String]! = NSBundle.mainBundle().pathsForResourcesOfType(".mp3", inDirectory: "Audio Assets")
+    let albumArtFilesPath: [String]! = NSBundle.mainBundle().pathsForResourcesOfType(".jpg", inDirectory: "Audio Assets")
+    var randomSongIndex = 0
+    var isPlaying = "nope"
+    
     // MARK: - IBOutlet properties
     
-    @IBOutlet weak var albumArtImageView: UIImageView!
-        /*{
+    @IBOutlet weak var albumArtImageView: UIImageView! {
         didSet {
-            /*
-            let directions: [UISwipeGestureRecognizerDirection] = [.Down, .Left, .Right]
-            let recognizers: [UISwipeGestureRecognizer] = directions.map { direction in
-            let gesture = UISwipeGestureRecognizer(target: self, action: "validateSwipeGesture")
-            gesture.direction = direction
-            self.someView.addGestureRecognizer(gesture)
-            }
-            */
+            self.albumArtImageView.userInteractionEnabled = true
         }
     }
-    */
-    
+    @IBOutlet weak var instructionsTextView: UITextView! {
+        didSet {
+            self.instructionsTextView.editable = false
+        }
+    }
     // MARK: - Local methods
     
-    func validateSwipeGesture(swipeGesture: UISwipeGestureRecognizer) {
-        switch swipeGesture.direction {
-        case UISwipeGestureRecognizerDirection.Down:
-            print("down")
-        case UISwipeGestureRecognizerDirection.Left:
-            print("left")
-        case UISwipeGestureRecognizerDirection.Right:
-            print("right")
-        default:
-            break
+    func initiateSoundPlayer() {
+        do {
+            songPlayer = try AVAudioPlayer(contentsOfURL: NSURL(fileURLWithPath: self.songFilesPath[self.randomSongIndex]))
+            songPlayer.prepareToPlay()
+        }
+        catch {
+            print("something has gone awfully wrong...")
         }
     }
     
-//    func validateSwipeGesture(swipeGesture: UISwipeGestureRecognizer) {
-//        if let isSwipeGesture = swipeGesture as? UISwipeGestureRecognizer {
-//            switch isSwipeGesture.direction {
-//            case UISwipeGestureRecognizerDirection.Down:
-//                print("down")
-//            case UISwipeGestureRecognizerDirection.Left:
-//                print("left")
-//            case UISwipeGestureRecognizerDirection.Right:
-//                print("right")
-//            default:
-//                break
-//            }
-//        }
-//    }
+    
+    func initiateSwipeRelatedGesturesToControlSongPlayback() {
+        /*
+        let directions: [UISwipeGestureRecognizerDirection] = [.Down, .Left, .Right]
+        let recognizers: [UISwipeGestureRecognizer] = directions.map { direction in
+        let gesture = UISwipeGestureRecognizer(target: self, action: "validateSwipeGesture")
+        gesture.direction = direction
+        self.someView.addGestureRecognizer(gesture)
+        }
+        */
+        let swipeDirections: [UISwipeGestureRecognizerDirection] = [.Left, .Right]
+        for swipeDirection in swipeDirections {
+            let gesture = UISwipeGestureRecognizer(target: self, action: "validateGestureType:")
+            gesture.direction = swipeDirection
+            self.view.subviews[0].addGestureRecognizer(gesture)
+        }
+    }
+    
+    func initiateTapGestureToPlayOrPauseSong() {
+        let tapGesture = UITapGestureRecognizer(target: self, action: "validateGestureType:")
+        self.view.subviews[0].addGestureRecognizer(tapGesture)
+    }
+    
+    func initiateTapAndHoldGestureToStopSong() {
+        let longPressGesture = UILongPressGestureRecognizer(target: self, action: "validateGestureType:")
+        longPressGesture.minimumPressDuration = 1.0
+        self.view.subviews[0].addGestureRecognizer(longPressGesture)
+    }
+    
+    func playOrPauseSong() {
+        self.albumArtImageView.image = UIImage(contentsOfFile: self.albumArtFilesPath[self.randomSongIndex])
+        if self.isPlaying == "nope" {
+            self.songPlayer.play()
+            self.isPlaying = "yup"
+        }
+        else {
+            self.songPlayer.pause()
+            self.isPlaying = "nope"
+        }
+    }
+    
+    func stopSong() {
+        self.albumArtImageView.image = UIImage(named: "defaultAlbumCover")
+        self.songPlayer.stop()
+        self.songPlayer.currentTime = 0
+    }
+    
+    func validateGestureType(gestureType: UIGestureRecognizer) {
+        if let isGestureSwipe = gestureType as? UISwipeGestureRecognizer {
+            switch isGestureSwipe.direction {
+            case UISwipeGestureRecognizerDirection.Left:
+                print("")
+            case UISwipeGestureRecognizerDirection.Right:
+                print("right")
+            default:
+                break
+            }
+        }
+        else if let _ = gestureType as? UILongPressGestureRecognizer {
+            self.stopSong()
+            
+        }
+        else if let _ = gestureType as? UITapGestureRecognizer {
+            self.playOrPauseSong()
+        }
+    }
+    
+    // MARK: - UIResponder methods override
+    
+    override func motionEnded(motion: UIEventSubtype, withEvent event: UIEvent?) {
+        if event?.subtype == UIEventSubtype.MotionShake {
+        }
+    }
     
 }
 
